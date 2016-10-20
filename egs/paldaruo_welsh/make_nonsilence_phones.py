eb88cfb93244eae7baa0c7a4601ed049e9c3c857
Dewi Bryn Jones <d.b.jones@bangor.ac.uk>
2016-10-20 11:43:39 +0100

    make_nonsilence_phones

---
+++ egs/paldaruo_welsh/make_nonsilence_phones.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import os,sets
+from argparse import ArgumentParser
+
+phoneset = set()
+phoneset_add = phoneset.add
+
+dest_dir = 'data/local/dict'
+
+with open(dest_dir + '/lexicon','rb') as lex:
+
+    for line in lex:
+        (key, val) = line.split(' ',1)
+        val = val.lstrip().rstrip()
+        phonemes = val.split(' ')
+
+        [x for x in phonemes if not (x in phoneset or phoneset_add(x))]
+
+sorted_phones = sorted(phoneset, key=lambda s: s.lower())
+
+with open(dest_dir + '/nonsilence_phones.txt','w') as nonsilence:
+    for phone in sorted_phones:
+        nonsilence.write(phone + '\n')
+
+str_phones = ', '.join(sorted_phones)
+print str_phones
