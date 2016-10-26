d33d541234946e56ec9e87ff3fef64e689c101b5
Dewi Bryn Jones <d.b.jones@bangor.ac.uk>
2016-10-26 13:58:10 +0100

    chwynnu linellau sy'n cynnwys tokens sydd ddim yn ein lexicon

--- egs/paldaruo_welsh/tokenize.py
+++ egs/paldaruo_welsh/tokenize.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python
 
-import sys, os, errno, re, codecs
+import sys, os, errno, re, codecs, utils
 from argparse import ArgumentParser
+
+lexicon = {}
+
+lexicon_dir = 'data/local/dict'
+
+with open(lexicon_dir + '/lexicon.txt') as lexicon_file:
+	for line in lexicon_file:
+		elements = line.rstrip().split(' ',1)
+		key = elements[0].replace('*/','')
+		lexicon[key]=elements[1]
+
 
 def tokenize_file(infile, outfile, **args):
 
@@ -11,13 +22,19 @@ def tokenize_file(infile, outfile, **args):
 		for line in in_file.readlines():
 			upper=line.rstrip('\n').upper()
 			tokens = tuple(re.findall(r"(?:^|\b).(?:\B.)*",upper))
-
-			upper=[]
+			tokens_out=[]
+			write_to_file=True
 			for token in tokens:
 				if token.isalpha():
-					upper.append(token)
-
-			out_file.write(' '.join(upper) + "\n")
+					if token in lexicon:
+						tokens_out.append(token)
+					else:
+						write_to_file=False
+						break
+			if write_to_file:
+				out_file.write(' '.join(tokens_out) + "\n")
+			else:
+				print "Skipped " + upper.encode('utf-8')
 	
 	out_file.close()
 
